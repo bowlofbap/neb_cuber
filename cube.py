0347402122192fe2d9b1bf04c0958665ffad9452
Daniel Yoo <daniel.yoo0523@gmail.com>
2021-08-24 20:37:53 -0400

    finished mvp

--- cube.py
+++ cube.py
@@ -5,14 +5,16 @@ import win32gui
 import numpy as np
 import pytesseract
 from PIL import ImageGrab
+from directkeys import PressKey, ReleaseKey, ClickKey, DIRECTIONS
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+path = "./ss/img.png"
 
 toplist, winlist = [], []
 def enum_cb(hwnd, results):
     winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
 
-def grab_screenshot():
+def sss():
     win32gui.EnumWindows(enum_cb, toplist)
 
     aurora = [(hwnd, title) for hwnd, title in winlist if 'aurora' in title.lower()]
@@ -29,29 +31,89 @@ def grab_screenshot():
     width, height = img.size
     
     # Setting the points for cropped image
-    left = 430
-    top = 255
-    right = 945
-    bottom = 570
+    left = 0
+    top = 0
+    right = 0
+    bottom = 0
     
     # Cropped image of above dimension
     # (It will not change original image)
-    im1 = img.crop((left, top, right, bottom))
+    #im1 = img.crop((left, top, right, bottom))
+    img.save(path)
+
+def grab_screenshot(l, t, r, b):
+    win32gui.EnumWindows(enum_cb, toplist)
+
+    aurora = [(hwnd, title) for hwnd, title in winlist if 'aurora' in title.lower()]
+    aurora = aurora[0]
+    hwnd = aurora[0]
+
+    win32gui.SetForegroundWindow(hwnd)
+    bbox = win32gui.GetWindowRect(hwnd)
+    img = ImageGrab.grab(bbox)
+
+    #crop image top left
+    # Size of the image in pixels (size of original image)
+    # (This is not mandatory)
+    width, height = img.size
+    
+    # Setting the points for cropped image
+    
+    # Cropped image of above dimension
+    # (It will not change original image)
+    im1 = img.crop((l, t, r, b))
     return cv2.cvtColor(np.array(im1), cv2.COLOR_RGB2BGR)
 
-def detect_page1(img):
+def move_and_click():
+    aurora = [(hwnd, title) for hwnd, title in winlist if 'aurora' in title.lower()]
+    aurora = aurora[0]
+    hwnd = aurora[0]
+    rect = win32gui.GetWindowRect(hwnd)
+    pyautogui.click(rect[0] + 430 + 50, rect[1] + 255 + 50)
+
+def detect_string_in_page(img, string):
     text = pytesseract.image_to_string(img)
-    if "Use an Alien Cube" in text:
-        return True
-    return False
+    if string in text:
+        return True, text
+    return False, text
  
+#poc
+'''
 def main():
-    img = grab_screenshot()
-    detected_page_1 = detect_page1(img)
+    print(text1)
     if detected_page_1:
         print("On page1")
+        ClickKey(DIRECTIONS["DOWN"])
+        ClickKey(DIRECTIONS["DOWN"])
+        ClickKey(DIRECTIONS["DOWN"])
+        ClickKey(DIRECTIONS["DOWN"])
+        ClickKey(DIRECTIONS["DOWN"])
+        ClickKey(DIRECTIONS["SPACE"])
+        time.sleep(1)
+        img2 = grab_screenshot(640, 325, 945, 570)
+        detected_page_2, text2 = detect_string_in_page(cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY), "] Nebulite")
+        print(text2)
+        if detected_page_2:
+            move_and_click()
+            print("On page2")
+        else:
+            print("Not Page 2")
     else:
         print("Not page1")
+'''
+def main():
+    detected = False
+    while not detected:
+        ClickKey(DIRECTIONS["SPACE"])
+        time.sleep(.2)
+        img = grab_screenshot(643, 383, 913, 445)
+        correct_page, return_text = detect_string_in_page(img, "Nebulite (")
+        if not correct_page:
+            print("Go to the Socket Master and select the neb you want to cube.")
+            return
+        detected, text1 = detect_string_in_page(img, "All Stats: +6%")
+        print(text1)
+        time.sleep(.1)
 
 if __name__ == "__main__":
     main()
---
+++ directkeys.py
@@ -0,0 +1,110 @@
+# direct inputs
+# source to this solution and code:
+# http://stackoverflow.com/questions/14489013/simulate-python-keypresses-for-controlling-a-game
+# http://www.gamespp.com/directx/directInputKeyboardScanCodes.html
+
+import ctypes
+import time
+
+SendInput = ctypes.windll.user32.SendInput
+
+DIRECTIONS = {
+    "ESC": 0x01,
+    "ENTER": 0x1C,
+    "LEFT": 0x4B,
+    "RIGHT": 0x4D,
+    "UP": 0x48,
+    "DOWN": 0x50,
+    "SPACE": 0x39,
+    "CTRL": 0x1D,
+    "ALT": 0x38,
+    "SHIFT": 0x2A,
+    "F1": 0x3B,
+    "F2": 0x3C,
+    "F3": 0x3D,
+    "F4": 0x3E,
+    "F5": 0x3F,
+    "F6": 0x40,
+    "F7": 0x41,
+    "F8": 0x42,
+    "F9": 0x43,
+    "F10": 0x44,
+    "Q": 0x10,
+    "W": 0x11,
+    "E": 0x12,
+    "R": 0x13,
+    "T": 0x14,
+    "Y": 0x15,
+    "U": 0x16,
+    "I": 0x17,
+    "O": 0x18,
+    "P": 0x19,
+    "A": 0x1E,
+    "S": 0x1F,
+    "D": 0x20,
+    "F": 0x21,
+    "G": 0x22,
+    "H": 0x23,
+    "J": 0x24,
+    "K": 0x25,
+    "L": 0x26,
+    "Z": 0x2C,
+    "X": 0x2D,
+    "C": 0x2E,
+    "V": 0x2F,
+    "B": 0x30,
+    "N": 0x31,
+    "M": 0x32,
+
+}
+
+# C struct redefinitions 
+PUL = ctypes.POINTER(ctypes.c_ulong)
+class KeyBdInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class HardwareInput(ctypes.Structure):
+    _fields_ = [("uMsg", ctypes.c_ulong),
+                ("wParamL", ctypes.c_short),
+                ("wParamH", ctypes.c_ushort)]
+
+class MouseInput(ctypes.Structure):
+    _fields_ = [("dx", ctypes.c_long),
+                ("dy", ctypes.c_long),
+                ("mouseData", ctypes.c_ulong),
+                ("dwFlags", ctypes.c_ulong),
+                ("time",ctypes.c_ulong),
+                ("dwExtraInfo", PUL)]
+
+class Input_I(ctypes.Union):
+    _fields_ = [("ki", KeyBdInput),
+                 ("mi", MouseInput),
+                 ("hi", HardwareInput)]
+
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
+
+# Actuals Functions
+
+def PressKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+def ReleaseKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+def ClickKey(hexKeyCode):
+    PressKey(hexKeyCode)
+    ReleaseKey(hexKeyCode)
